cd6ef365fda1cc7fc5173fa5d4ddde3d75a8ea1d
Soufiane Tafahi <104545648+soufianeTafahi@users.noreply.github.com>
2022-04-28 04:46:38 +0000

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+/** ===================== typing animation ====================== */
+
+var typed = new Typed(".typing",{
+    strings:["","Web Devloper","Web Designer","Desktop Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
